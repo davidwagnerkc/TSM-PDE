d5471e807b70e7659224a1a28978d2d3046fa2d9
David Wagner <david@wagnerkc.com>
2026-02-01 06:56:40 -0600

    Repro scripts

--- scripts/train_generation.sh
+++ scripts/train_generation.sh
@@ -8,19 +8,20 @@ export PYTHONPATH="$PWD:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0,1,2,3  # ,4,5,6,7
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 export HAIKU_FLATMAPPING=0
+
 export STORAGE_PATH=$PSCRATCH/cfd
 export MODEL_NAME=dns_2048x2048_train_${TRAINING_DATA_SEED}
 export PREDICTDATA=models/${MODEL_NAME}/predict.nc
 
-python -u tsm/dns_warmup.py \
-  --model_predict_steps=16 \
-  --delta_time=0.007012483601762931 \
-  --num_samples=16 \
-  --inner_steps=1 \
-  --train_init_random_seed=$TRAINING_DATA_SEED \
+python -u tsm/dns_generate.py \
   --model_input_size=2048 \
-  --warmup_time=40.0 \
-  --simulation_time=1.0 \
+  --model_encode_steps=1 \
+  --model_predict_steps=32 \
+  --simulation_time=80.0 \
+  --delta_time=0.007012483601762931 \
+  --predict_split="$STORAGE_PATH/$PREDICTDATA" \
+  --predict_result="train.nc" \
+  --explicit_inner_steps 1 \
   --output_dir="$STORAGE_PATH/models/$MODEL_NAME" \
   --gin_file="tsm/configs/implicit_diffusion_dns_config.gin" \
   --gin_file="tsm/configs/kolmogorov_forcing.gin"
